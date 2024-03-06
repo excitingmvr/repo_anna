712dd2d7a0e93b05ab17322891e8c0bcbe332487
excitingmvr <excitingmvr@gmail.com>
2024-03-06 15:44:41 +0900

    first

---
+++ anna/src/main/java/com/junefw/AnnaApplication.java
@@ -0,0 +1,13 @@
+package com.junefw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnaApplication.class, args);
+	}
+
+}
